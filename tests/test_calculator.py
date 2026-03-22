7531bede08b20a37dd9d1ae8c058cd6a3dc8818e
Sinkit Kumar <168271350+Sinkitsingh@users.noreply.github.com>
2026-03-22 20:13:45 +0530

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,7 @@
-from app.calculator import add, substract
+from app.calculator import add, subtract
 
 def test_add():
   assert add(2,3) == 5
 
-def test_substract():
-  assert substract(5,3) == 2
+def test_subtract():
+  assert subtract(5,3) == 2
